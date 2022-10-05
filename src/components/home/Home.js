fafaf113d6b0761f1a6aee77b82e5171d875c4ed
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-10-05 10:31:37 +0330

    home component devide to left and right

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,3 +1,5 @@
+import BodyLeft from "./BodyLeft";
+import BodyRight from "./BodyRight";
 import "./home.css";
 
 const Home = () => {
@@ -5,19 +7,8 @@ const Home = () => {
     <div className="home">
       <div className="containar">
         <div className="row home-content">
-          <div className="col-lg-6 p-5">
-            <div className="left-containar">
-              <h5 className="fw-light">SO, YOU WANT TO TRAVEL TO</h5>
-              <h1>SPACE</h1>
-              <p>
-                Let’s face it; if you want to go to space, you might as well
-                genuinely go to outer space and not hover kind of on the edge of
-                it. Well sit back, and relax because we’ll give you a truly out
-                of this world experience!
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-6 p-5">fvv</div>
+          <BodyLeft/>
+          <BodyRight/>
         </div>
       </div>
     </div>
