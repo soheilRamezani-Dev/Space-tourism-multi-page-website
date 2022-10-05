3bcdc0010f59a506ba38d12148265f0c71e9dc46
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-10-05 09:21:43 +0330

    home body left side complete

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,27 @@
-import './home.css'
+import "./home.css";
 
 const Home = () => {
-    return <div className="home">
-
+  return (
+    <div className="home">
+      <div className="containar">
+        <div className="row home-content">
+          <div className="col-lg-6 p-5">
+            <div className="left-containar">
+              <h5 className="fw-light">SO, YOU WANT TO TRAVEL TO</h5>
+              <h1>SPACE</h1>
+              <p>
+                Let’s face it; if you want to go to space, you might as well
+                genuinely go to outer space and not hover kind of on the edge of
+                it. Well sit back, and relax because we’ll give you a truly out
+                of this world experience!
+              </p>
+            </div>
+          </div>
+          <div className="col-lg-6 p-5">fvv</div>
+        </div>
+      </div>
     </div>
-}
- 
+  );
+};
+
 export default Home;
