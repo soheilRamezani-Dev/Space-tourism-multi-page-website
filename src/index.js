b277b860f6272d9089ff87d857d8b3bc8587050c
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-10-05 07:17:20 +0330

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import App from "./App";
 import reactDom from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const root = reactDom.createRoot(document.getElementById('root'));
 
