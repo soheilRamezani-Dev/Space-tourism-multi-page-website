0f7922854ced1449aa9870c882c1442bf696617e
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-10-05 16:36:03 +0330

    technologe left complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import Destination from "./components/destination/Destination";
 import Header from "./components/Header/Header";
 import Home from "./components/home/Home";
 import Crew from "./components/crew/Crew";
+import Technology from "./components/technology/Technology";
 
 const App = () => {
     return <>
     <Header/>
-    <Crew/>
+    <Technology/>
     </>
 }
  
--- src/components/technology/Technology.js
+++ src/components/technology/Technology.js
@@ -1,9 +1,12 @@
+import TechnologyLeft from "./technology-left/TechnologyLeft";
+import './technology.css'
+
 const Technology = () => {
   return (
-    <div className="crew">
+    <div className="technology">
       <div className="containar">
         <div className="row">
-
+          <TechnologyLeft />
         </div>
       </div>
     </div>
