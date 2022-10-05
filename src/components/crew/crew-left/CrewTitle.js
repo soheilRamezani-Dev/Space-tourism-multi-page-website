f0c14ab206a8e9900745361c4d1a35b936c95a9e
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-10-05 15:59:31 +0330

    crew title change

--- src/components/crew/crew-left/CrewTitle.js
+++ src/components/crew/crew-left/CrewTitle.js
@@ -2,7 +2,7 @@ const CrewTitle = () => {
   return (
     <h5 className="w-75 d-flex justify-content-center">
       <span className="text-secondary m-2 ms-5">02 </span>
-      <span className="fw-light m-2"> Pick your destination</span>
+      <span className="fw-light m-2"> Meet your crew</span>
     </h5>
   );
 };
