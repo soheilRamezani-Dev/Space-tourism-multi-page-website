4ec187c07f855268032f5033a6b313f51487fe93
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-10-05 15:56:53 +0330

    technology component init

---
+++ src/components/technology/Technology.js
@@ -0,0 +1,13 @@
+const Technology = () => {
+  return (
+    <div className="crew">
+      <div className="containar">
+        <div className="row">
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Technology;
