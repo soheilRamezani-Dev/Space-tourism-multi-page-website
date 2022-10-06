11e660b8a56f73ce3906cac86febc1aa427d5dcd
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-10-06 18:09:13 +0330

    home explore button complete

--- src/components/home/BodyRight.js
+++ src/components/home/BodyRight.js
@@ -1,12 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 const BodyRight = () => {
   return (
     <div className="col-lg-6 p-5">
-      <div
-        role="button"
-        className="explore-button rounded-circle bg-white d-flex align-items-center justify-content-center"
-      >
-        <span>EXPLORE</span>
-      </div>
+      <NavLink to="/destination">
+        <div
+          role="button"
+          className="explore-button rounded-circle bg-white d-flex align-items-center justify-content-center"
+        >
+          <span>EXPLORE</span>
+        </div>
+      </NavLink>
     </div>
   );
 };
