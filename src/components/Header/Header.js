cbfcd2a3c5c281a6f2aa34606fb48c1e498fd84f
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-10-07 10:25:47 +0330

    header template responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,33 +6,34 @@ const Header = () => {
   return (
     <>
       <section className="header">
-        <nav class="navbar navbar-expand-lg ">
+        <nav class="navbar navbar-dark navbar-expand-lg ">
           <div class="header-container">
             <div className="row">
-              <div className="logo col-lg-2 d-flex justify-content-center mt-2">
+              <div className="logo col-2 d-flex justify-content-center mt-2">
                 <a class="navbar-brand" href="#">
-                  <img src={process.env.PUBLIC_URL + "/images/shared/logo.svg"} />
+                  <img
+                    src={process.env.PUBLIC_URL + "/images/shared/logo.svg"}
+                  />
                 </a>
               </div>
-              <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div className="line col-lg-4"></div>
-              <div
-                class="navbar-items collapse navbar-collapse col-lg-6 d-flex justify-content-center"
-                id="navbarSupportedContent"
-              >
+              <div className="d-block d-md-none col-10 d-flex flex-row-reverse">
+                <button
+                  class="navbar-toggler"
+                  type="button"
+                >
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              </div>
+
+              <div className="d-none d-lg-block line col-lg-3"></div>
+              <div class="navbar-items col-lg-7 col-md-10 d-flex justify-content-center d-none d-md-block">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <NavLink to="/" end className={({isActive})=> isActive ? "active" : ""}>
+                    <NavLink
+                      to="/"
+                      end
+                      className={({ isActive }) => (isActive ? "active" : "")}
+                    >
                       <span className="nav-link">
                         <span className="fw-bold">00</span>
                         <span className="fw-light"> HOME</span>
@@ -40,7 +41,10 @@ const Header = () => {
                     </NavLink>
                   </li>
                   <li class="nav-item">
-                    <NavLink to="destination" className={({isActive})=>isActive ? "active" : ""}>
+                    <NavLink
+                      to="destination"
+                      className={({ isActive }) => (isActive ? "active" : "")}
+                    >
                       <span className="nav-link">
                         <span className="fw-bold">01</span>
                         <span className="fw-light"> DESTINATION</span>
@@ -48,7 +52,10 @@ const Header = () => {
                     </NavLink>
                   </li>
                   <li class="nav-item">
-                    <NavLink to="crew" className={({isActive})=>isActive ? "active" : ""}>
+                    <NavLink
+                      to="crew"
+                      className={({ isActive }) => (isActive ? "active" : "")}
+                    >
                       <span className="nav-link">
                         <span className="fw-bold">02</span>
                         <span className="fw-light"> CHEW</span>
@@ -56,7 +63,10 @@ const Header = () => {
                     </NavLink>
                   </li>
                   <li class="nav-item">
-                  <NavLink to="technology" className={({isActive})=>isActive ? "active" : ""}>
+                    <NavLink
+                      to="technology"
+                      className={({ isActive }) => (isActive ? "active" : "")}
+                    >
                       <span className="nav-link">
                         <span className="fw-bold">03</span>
                         <span className="fw-light"> TECHNOLOGY</span>
